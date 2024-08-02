282c6b20f23c2e2421eda4f6d1a2c82698c12fb9
RSD-Jm <jose_planteras@ring-group.jp>
2024-08-02 11:23:10 +0800

    Enh: added react-native-gifted-charts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,83 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React, { useEffect } from "react";
 import { Colors } from "@/constants/Colors";
 import { Stack, useNavigation } from "expo-router";
 import Header from "@/components/header";
+import { PieChart } from "react-native-gifted-charts";
 
 export default function Page() {
+  const pieData = [
+    {
+      value: 47,
+      color: "#009FFF",
+      gradientCenterColor: "#006DFF",
+      focused: true,
+    },
+    { value: 40, color: "#93FCF8", gradientCenterColor: "#3BE9DE" },
+    { value: 16, color: "#BDB2FA", gradientCenterColor: "#8F80F3" },
+    { value: 3, color: "#FFA5BA", gradientCenterColor: "#FF7F97" },
+  ];
+
   return (
     <>
       <Stack.Screen options={{ header: () => <Header /> }} />
       <View
         style={{
           backgroundColor: Colors.BLACK,
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
+          height: "100%",
+          paddingTop: 80,
+          paddingHorizontal: 20,
         }}
       >
-        <Text style={style.text}>Index Page</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View>
+              <Text style={[style.text, { fontSize: 20 }]}>
+                My <Text style={{ fontSize: 20 }}>Expenses</Text>
+              </Text>
+              <Text style={[style.text, { fontSize: 30, fontWeight: "bold" }]}>
+                $ 1,475.
+                <Text style={{ fontWeight: 400, fontSize: 20 }}>00</Text>
+              </Text>
+            </View>
+            <View style={{ paddingVertical: 20, alignItems: "center" }}>
+              <PieChart
+                data={pieData}
+                donut
+                showGradient
+                sectionAutoFocus
+                focusOnPress
+                semiCircle
+                radius={70}
+                innerRadius={55}
+                innerCircleColor={Colors.BLACK}
+                centerLabelComponent={() => {
+                  return (
+                    <View
+                      style={{ justifyContent: "center", alignItems: "center" }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 22,
+                          color: "white",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        47%
+                      </Text>
+                    </View>
+                  );
+                }}
+              />
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </>
   );
